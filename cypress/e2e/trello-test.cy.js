2c3c3e83d254e8091a9d4c292ffcbbf9782e7cd9
Whitney Lea <whitslea@gmail.com>
2023-05-30 14:25:22 -0400

    Cypress For Kargo (Edit 1.0)

--- cypress/e2e/trello-test.cy.js
+++
@@ -1,7 +0,0 @@
-/// <reference types ="cypress" />
-
-it('Verify create a new card works', () => {
-    cy.visit('https://www.tropicalsmoothiecafe.com/');
-    cy.get('#APjFqb').type('Early Nintendo{Enter}');
-    cy.contains('Images').click();
-});
