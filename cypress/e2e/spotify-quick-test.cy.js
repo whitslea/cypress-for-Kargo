91210050e813b2088ff283f569a84bdad43f4b70
Whitney Lea <whitslea@gmail.com>
2023-05-30 14:15:39 -0400

    Delete spotify-quick-test.cy.js

--- cypress/e2e/spotify-quick-test.cy.js
+++
@@ -1,7 +0,0 @@
-/// <reference types ="cypress" />
-
-it('Search for a song and verify play button works', () => {
-    cy.visit('https://open.spotify.com/');
-    cy.get('#APjFqb').type('Early Nintendo{Enter}');
-    cy.contains('Images').click();
-});
